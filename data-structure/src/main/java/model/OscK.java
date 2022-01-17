f90fce0acd268af8502e95de14f259056ef371e3
dwd <weidong.du@opple.com>
2022-01-17 18:00:41 +0800

    Update OscK.java

--- data-structure/src/main/java/model/OscK.java
+++ data-structure/src/main/java/model/OscK.java
@@ -8,9 +8,11 @@ import java.math.BigDecimal;
 public class OscK extends BaseK {
     private long start;//开始
     private long end;//结束  (默认 最新ts)
+
     private BigDecimal[] priceOsc;//每日波动率
+
     private BigDecimal[] upDownBlock;//上下轨区间 0x(ts) 1y(price)
-    
+
     private BigDecimal[] upDownLine;//上下轨line, length=8
                                     //高点拟合 up (upx1,upy1) (upx2,upy2)
                                     //低点拟合 dn (dnx1,dny1) (dnx2,dny2)
